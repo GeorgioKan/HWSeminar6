5a78063c50713d1f7bbede2734a2be7dd4278f75
Gerorge <logovas4@gmail.com>
2023-02-01 20:07:04 +0300

    Task1 added

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int NumberInput()
+{
+    int number;
+    while (true)
+    {
+        string? str = Console.ReadLine();
+        if (int.TryParse(str, out number))
+        {
+            break;
+        }
+        else
+            Console.WriteLine("Введено не число. Попробуйте снова: ");
+    }
+
+    return number;
+}
+
+int counter = 0, M = 5;
+Console.WriteLine("Введите М чисел: ");
+for (int i = 0; i < M; i++)
+{
+    int num = NumberInput();
+    if (num > 0)
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);
