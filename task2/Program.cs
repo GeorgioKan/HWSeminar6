7cafd8f81c1ff7e70bc6078dbd9c076cf8793f6d
Gerorge <logovas4@gmail.com>
2023-02-01 20:50:00 +0300

    Task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Function(double k1, double k2, double b1, double b2)
+{
+    double x;
+    double y;
+    x = (b1 - b2) / (k2 - k1);
+    y = 9 * x + 4;
+    System.Console.WriteLine(x);
+    System.Console.WriteLine(y);
+}
+
+double b1 = 2, k1 = 5, b2 = 4, k2 = 9;
+Function(k1,k2,b1,b2);
